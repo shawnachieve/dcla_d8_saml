c80092eba667bd682bbaf9a8a51cde001b4e677c
Shawn S. Smiley <shawn.smiley@achieveinternet.com>
2019-08-20 20:18:51 -0700

    Configuring idp metadata.

---
+++ simplesamlphp/metadata/saml20-idp-remote.php
@@ -0,0 +1,36 @@
+<?php
+$metadata['drupalcampla19demotest-idp'] = array (
+  'entityid' => 'drupalcampla19demotest-idp',
+  'contacts' =>
+    array (
+    ),
+  'metadata-set' => 'saml20-idp-remote',
+  'SingleSignOnService' =>
+    array (
+      0 =>
+        array (
+          'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
+          'Location' => 'https://sso.jumpcloud.com/saml2/dclatest',
+        ),
+    ),
+  'SingleLogoutService' =>
+    array (
+    ),
+  'ArtifactResolutionService' =>
+    array (
+    ),
+  'NameIDFormats' =>
+    array (
+      0 => 'urn:oasis:names:tc:SAML:1.0:nameid-format:unspecified',
+    ),
+  'keys' =>
+    array (
+      0 =>
+        array (
+          'encryption' => false,
+          'signing' => true,
+          'type' => 'X509Certificate',
+          'X509Certificate' => 'MIIEDzCCAvegAwIBAgIJAJdQZXSrWOK6MA0GCSqGSIb3DQEBCwUAMIGdMQswCQYDVQQGEwJVUzETMBEGA1UECAwKQ2FsaWZvcm5pYTESMBAGA1UEBwwJU2FuIERpZWdvMRkwFwYDVQQKDBBBY2hpZXZlIEludGVybmV0MRkwFwYDVQQDDBBBY2hpZXZlIEludGVybmV0MS8wLQYJKoZIhvcNAQkBFiBzaGF3bi5zbWlsZXlAYWNoaWV2ZWludGVybmV0LmNvbTAeFw0xOTA4MjEwMjQ2MDhaFw0yMDA4MjAwMjQ2MDhaMIGdMQswCQYDVQQGEwJVUzETMBEGA1UECAwKQ2FsaWZvcm5pYTESMBAGA1UEBwwJU2FuIERpZWdvMRkwFwYDVQQKDBBBY2hpZXZlIEludGVybmV0MRkwFwYDVQQDDBBBY2hpZXZlIEludGVybmV0MS8wLQYJKoZIhvcNAQkBFiBzaGF3bi5zbWlsZXlAYWNoaWV2ZWludGVybmV0LmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMOeDgwx2ofZ9sTfXuDeuBoPldIFqekGxwaJKemfkGqx3/uPfq5BUheVKKSQt9AoGPMJXsBrDglLyvxbdJT30mSbE6VxlmmIfMB570TfzCT4RdWhxuu557nWz0lIU9YqPvXGSR0MZG76VU7+r4f/mpypT4NW6FwiDWbNjjc8G39l/MrSMkvX6MiLyJ8ksZKU+BjuQiZKjWDjXbndUVVN04Kce68PlWeFO/tZl+258Ygj4lnd2j+GPpdj44M1I1o+ZXMGHJMYBSz+OWr+Yn52Jsq2XmG3hzWCahuvuCYOoCi9++5T2zlBJUUTwDmaHRV+4VxGt62gvWBlQNXCMQcFYQcCAwEAAaNQME4wHQYDVR0OBBYEFAHQmnBQTV1spr9D2I2TYnLuWhUoMB8GA1UdIwQYMBaAFAHQmnBQTV1spr9D2I2TYnLuWhUoMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQELBQADggEBAFvVPHpS5gNMgDctXCVfc3h0Vbbb7mzu85TAcWCD4mLUuVzLT6j9JbxWQb0xorP5hwfvK17+VS03pb4UgZv0en3gOt9cwQ/9jc2HB9AaEutHtsiO+wZ79MpWDjGrX+jMs6NgTQaQkSurE5hlugdSdkj6hd/QCm2Hp4vM7mkZOmpcsBSejX9YtSDL2mFh8af7SxejDgeyaNhHR+RQV6SxYo0Gp/ds1SoQoUgqqYNzckPDQb4Up94yeHuikFvIru9bDZE/rqnh0B1fwZuKhjIWUuI/F+5hKmv3R5NN8a/lGux8yvKn1/BF1jO0XC1yw+5ikN6GKMvKtspNlI73pZGxVAY=',
+        ),
+    ),
+);
